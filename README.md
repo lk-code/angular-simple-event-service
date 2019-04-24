4a7c0b513f20ca8b68398576d009cdfac46d4d36
Lars Krämer <lars.kraemer@live.de>
2019-04-24 16:08:11 +0200

    - complete EventService

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# angular-simple-event-service
-a simple angular service for event handling (trigger and subscribe)
+# Simple Angular EventService
+
+## Setup
+1. copy the two service files to the service-directory in your angular app.
+2. inject the event-Service in your component, service, etc,
+
+
+```
+import { EventService } from '../event/event.service';
+
+constructor(private eventService: EventService) {
+    this.subscribeEvents();
+    this.triggerEvents();
+}
+
+subscribeEvents() {
+    this.eventService.Subscribe("yourapp:some-event", function(data) {
+        console.log(data); // => "hello world"
+    });
+}
+
+triggerEvents() {
+    this.eventService.Trigger("yourapp:some-event", "hello world");
+}
+```
+
+## Trigger
+
+name: string - an unique event-key
+
+data:? any - you can send a lot of things through the Trigger-method - strings, numbers, objects, etc.
+
+## Subscribe
+
+name: string - the event-name to listen
+
+handler: Function - the method, which should be executed if the event is triggered
---
+++ src/app/services/event/event.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * Simple Angular EventService
+ * by Lars Krämer - lkraemer.de
+ * 
+ * ##################################################################################
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2019 Lars Krämer
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+  events: { name: string, handler: Function }[] = [];
+
+  constructor() {
+
+  }
+
+  /**
+   * 
+   * @param name 
+   * @param handler 
+   */
+  public Subscribe(name: string, handler: Function) {
+    this.events.push({ name, handler });
+  }
+
+  /**
+   * 
+   * @param name 
+   * @param data 
+   */
+  public Trigger(name: string, data?: any) {
+    let requestedEvents = this.events.filter(e => e.name == name);
+
+    for (let requestedEvent of requestedEvents) {
+      requestedEvent.handler(data);
+    }
+  }
+}
